ae6d8b2a9883f82b778a19a388fc5b90a4e5ef06
HiroshiNakagawa <rosshi96@gmail.com>
2022-05-25 07:24:53 +0900

    ♻️ コードを移動した

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,19 @@
 import "src/styles/globals.css";
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta
+          name="description"
+          content="ログハウスの情報検索をサポートするサイト"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
       <MantineProvider
         withGlobalStyles
         withNormalizeCSS
