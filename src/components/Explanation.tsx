3ad9def81e96f0aa4c07b325a5e62b6152fbd0dd
HiroshiNakagawa <rosshi96@gmail.com>
2022-05-26 01:48:32 +0900

    ✨ 仮のページ説明コンポーネントを追加

---
+++ src/components/Explanation.tsx
@@ -0,0 +1,9 @@
+export const Explanation = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-red-500 underline">
+        ログハウス情報の検索をサポートするよ！
+      </h1>
+    </div>
+  );
+};
