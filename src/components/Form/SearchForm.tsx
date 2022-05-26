8070fa981cde0c25a0e1a8eec0dca8b59e0deff5
HiroshiNakagawa <rosshi96@gmail.com>
2022-05-27 08:56:55 +0900

    ✨ フォーム入力中に×ボタンでテキストを消せるようになった

--- src/components/Form/SearchForm.tsx
+++ src/components/Form/SearchForm.tsx
@@ -1,11 +1,21 @@
-import { Box, Button, Group, TextInput } from "@mantine/core";
+import { Box, Button, CloseButton, Group, TextInput } from "@mantine/core";
 import { SearchFormProps } from "./type";
 
 export const SearchForm = (props: SearchFormProps) => {
+  const cancelButton =
+    props.form.values.word.length > 0 ? (
+      <CloseButton
+        title="Close popover"
+        size="sm"
+        iconSize={20}
+        onClick={props.form.reset}
+      />
+    ) : null;
   return (
     <Box sx={{ maxWidth: 600 }} mx="auto">
       <form onSubmit={props.form.onSubmit((values) => console.log(values))}>
         <TextInput
+          type="search"
           required
           label="検索ワード"
           placeholder="検索ワードを入力してください"
@@ -14,8 +24,8 @@ export const SearchForm = (props: SearchFormProps) => {
           onChange={(e) => {
             props.form.setFieldValue("word", e.currentTarget.value);
           }}
+          rightSection={cancelButton}
         />
-
         <Group position="right" mt="md">
           <Button type="submit">検索</Button>
         </Group>
